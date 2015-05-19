6fb48fe52585bd52f253b2ecfb15ae31662f52eb
chasemc67 <chasemc67@gmail.com>
2015-05-18 18:27:30 -0600

    removed modal box

--- index.php
+++ index.php
@@ -107,19 +107,6 @@
             <a class="button button-primary" href="#">Show More</a>
           </div>
 
-          <!-- Modal dialgo box test -->
-
-          <div id="openModal" class="modalDialog">
-            <div>
-              <a href="#close" title="Close" class="close">X</a>
-              <h2>Modal Box</h2>
-              <p>This is a sample modal box that can be created using the powers of CSS3.</p>
-              <p>You could do a lot of things here like have a pop-up ad that shows when your website loads, or create a login/register form for users.</p>
-            </div>
-          </div>
-
-          <!--                       -->
-
           <div class = "four columns">
             <a class="button button-primary" href="#">Show More</a>
           </div>
