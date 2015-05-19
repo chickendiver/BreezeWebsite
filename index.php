d650450810aaa63866ad708b7ccb342531ccedc2
Bradley <bpoulett@ualberta.ca>
2015-05-19 12:54:12 -0600

    Added breaks to services alternate content

--- index.php
+++ index.php
@@ -99,14 +99,14 @@
               <p>Let us set up your new technology for you. You tell us what you want it to do, and we make it work. Sit back, relax, and watch the magic happen.</p>
             </div>
             <div id = "setupMore" style="display:none;">
-             <p>Our gurus set-up:
--	Printers
--	Mobile devices (iPads, iPhones, and other tablets and smartphones)
--	Home automation systems
--	New computers
--	Data transfers from your old devices to your new ones
+                <p><br>Our gurus set-up:
+<br><br>-	Printers
+<br>-	Mobile devices (iPads, iPhones, and other tablets and smartphones)
+<br>-	Home automation systems
+<br>-	New computers
+<br>-	Data transfers from your old devices to your new ones
 
-                 Don’t see what you want on the list? Request a free consultation using the contact form below, and one of our gurus will be more than happy to help.
+<br><br>Don’t see what you want on the list? Request a free consultation using the contact form below, and one of our gurus will be more than happy to help.
 </p> 
             </div>
           </div>
@@ -115,14 +115,14 @@
               <p>From basic computer skills, to software like Photoshop and more, our patient and skilled gurus will teach you what you want to know. In a casual, one-on-one atmosphere, you set the pace. No more frustrating classes!</p>
             </div>
             <div id = "lessonMore" style="display:none;">
-            <p>Our gurus can teach you how to:
--	Transfer data from cameras, phones, and tablets, to your computer
--	Synchronize your files across your devices (iCloud, Google Drive, Microsoft OneDrive, Dropbox, etc.)
--	Edit your photos and videos using software like Adobe Photoshop, Adobe Premier, iPhoto, and iMovie
--	Navigate your computer’s basic functionalities with ease
--   Use tools like email and web browsing
+            <p><br>Our gurus can teach you how to:
+<br><br>-	Transfer data from cameras, phones, and tablets, to your computer
+<br>-	Synchronize your files across your devices (iCloud, Google Drive, Microsoft OneDrive, Dropbox, etc.)
+<br>-	Edit your photos and videos using software like Adobe Photoshop, Adobe Premier, iPhoto, and iMovie
+<br>-	Navigate your computer’s basic functionalities with ease</span>
+<br>-   Use tools like email and web browsing
                 
-                Don’t see what you want on the list? Request a free consultation using the contact form below, and one of our gurus will be more than happy to help.</p>
+<br><br>Don’t see what you want on the list? Request a free consultation using the contact form below, and one of our gurus will be more than happy to help.</p>
             </div>
           </div>
           <div class = "four columns">
@@ -130,11 +130,12 @@
                <p>Have a trusted piece of technology that just isn’t working the way it used to? Let us know what the problem is, and we’ll take care of the rest.</p>
               </div>
               <div id = "repairMore" style="display:none;">
-                <p>Our gurus will take care of:
--	Speeding up slow computers
--	Recovering/replacing broken or damaged hard-drives
--	Virus removal (and anti-virus installation)
-</p>
+                <p><br>Our gurus will take care of:
+<br><br>-	Speeding up slow computers
+<br>-	Recovering/replacing broken or damaged hard-drives
+<br>-	Virus removal (and anti-virus installation)
+                
+<br><br>Don’t see what you want on the list? Request a free consultation using the contact form below, and one of our gurus will be more than happy to help.</p>
               </div>
           </div>
           </div>
