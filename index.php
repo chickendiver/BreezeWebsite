487867f445ab0a9110d20c33906d937a6c881b3e
chasemc67 <chasemc67@gmail.com>
2015-08-03 15:32:26 -0600

    Halfway done the site redesign here. if you want to see the old site, clone from the commit before this one

--- index.php
+++ index.php
@@ -69,15 +69,21 @@ window._fbq = window._fbq || [];
               
           </div> <!-- End Container -->
       </div>
+
+
     <div class="twelve columns">
         <div class = "text container">
             <h1>Get Comfortable</h1>
             <h2>This is going to be a breeze.</h2>
-            <div class="scroll down" style="float: right; margin-top: 20%; display: none;">
-              <h2>Scroll Down</h2>
-            </div>
         </div>
+
+        <picture>
+          <img id="heads" src="./images/landing_floating_heads_crop_1.png">
+        </picture>
     </div>
+
+     
+
   </div> <!-- End Band -->
 
 
@@ -109,141 +115,20 @@ window._fbq = window._fbq || [];
       <div class="container">
         <div class="row">
           <div class="twelve columns">
-            <h5>What We Do</h5>
-          </div>
-        </div>
-        
-          
-        <!-- Mobile services begin -->
-        <div class = "row" id = "servicesMColumns">
-          <div class = "four columns">
-              <h5>Setups</h5>
-              <div id = "setupLessM">
-              <p>Let us set up your new technology for you. You tell us what you want it to do, and we make it work. Sit back, relax, and watch the magic happen.</p>
-            </div>
-            <div id = "setupMoreM" style="display:none;text-align:left;">
-                <p>Our tech gurus set-up:
-<br><br>•&nbsp;Printers
-<br>•&nbsp;Mobile devices (iPads, iPhones, and other tablets and smartphones)
-<br>•&nbsp;Home automation systems
-<br>•&nbsp;New computers
-<br>•&nbsp;Data transfers from your old devices to your new ones
-
-<br><br>Don’t see what you want on the list? Request a free consultation using the contact form below, and one of our gurus will be more than happy to help.
-</p> 
-            </div>
-            <button  id="setupBtnM">Show More</button>
-          </div>
-          <div class = "four columns">
-            <h5>Lessons</h5>
-            <div id = "lessonLessM">
-              <p>From basic computer skills, to software like Photoshop and more, our patient and skilled gurus will teach you what you want to know. In a casual, one-on-one atmosphere, you set the pace. No more frustrating classes!</p>
-            </div>
-            <div id = "lessonMoreM" style="display:none;text-align:left;">
-            <p>Our tech gurus can teach you how to:
-<br><br>•&nbsp;Transfer data from cameras, phones, and tablets, to your computer
-<br>•&nbsp;Synchronize your files across your devices (iCloud, Google Drive, Microsoft OneDrive, Dropbox, etc.)
-<br>•&nbsp;Edit your photos and videos using software like Adobe Photoshop, Adobe Premier, iPhoto, and iMovie
-<br>•&nbsp;Navigate your computer’s basic functionalities with ease</span>
-<br>•&nbsp;Use tools like email and web browsing
-                
-<br><br>Don’t see what you want on the list? Request a free consultation using the contact form , and one of our gurus will be more than happy to help.</p>
-            </div>  
-            <button  id="lessonBtnM">Show More</button>
-          </div>
-          <div class = "four columns" style = "margin-bottom:15%;">
-               <h5>Repairs and Tune-Ups</h5>
-               <div id = "repairLessM">
-               <p>Have a trusted piece of technology that just isn’t working the way it used to? Let us know what the problem is, and we’ll take care of the rest.</p>
-              </div>
-              <div id = "repairMoreM" style="display:none;text-align:left;">
-                <p>Our tech gurus will take care of:
-<br><br>•&nbsp;Speeding up slow computers
-<br>•&nbsp;Recovering/replacing broken or damaged hard-drives
-<br>•&nbsp;Virus removal (and anti-virus installation)
-                
-<br><br>Don’t see what you want on the list? Request a free consultation using the contact form below, and one of our gurus will be more than happy to help.</p>
-              </div>
-              <button  id="repairBtnM">Show More</button>
-          </div>
-    </div>
-    
-        <!-- Mobile services end -->
-    
-        <!-- Desktop services begin -->
-        <div class = "row" id = "servicesDesktopHeaders">
-    
-        <div class = "four columns">
-            <h5>Setups</h5>
-        </div>
-        <div class = "four columns">
-            <h5>Lessons</h5>
-        </div>
-        <div class = "four columns">
-            <h5>Repairs and Tune-Ups</h5>
-        </div>
-        </div>
-    
-        <div class = "row" id = "servicesDesktopContent">
-          <div class = "four columns" >
-            <div id = "setupLess">
-              <p>Let us set up your new technology for you. You tell us what you want it to do, and we make it work. Sit back, relax, and watch the magic happen.</p>
-            </div>
-            <div id = "setupMore" style="display:none;text-align:left;">
-                <p>Our tech gurus set-up:
-<br><br>•&nbsp;Printers
-<br>•&nbsp;Mobile devices (iPads, iPhones, and other tablets and smartphones)
-<br>•&nbsp;Home automation systems
-<br>•&nbsp;New computers
-<br>•&nbsp;Data transfers from your old devices to your new ones
-
-<br><br>Don’t see what you want on the list? Request a free consultation using the contact form below, and one of our gurus will be more than happy to help.
-</p> 
-            </div>
-          </div>
-          <div class = "four columns">
-            <div id = "lessonLess">
-              <p>From basic computer skills, to software like Photoshop and more, our patient and skilled gurus will teach you what you want to know. In a casual, one-on-one atmosphere, you set the pace. No more frustrating classes!</p>
-            </div>
-            <div id = "lessonMore" style="display:none;text-align:left;">
-            <p>Our tech gurus can teach you how to:
-<br><br>•&nbsp;Transfer data from cameras, phones, and tablets, to your computer
-<br>•&nbsp;Synchronize your files across your devices (iCloud, Google Drive, Microsoft OneDrive, Dropbox, etc.)
-<br>•&nbsp;Edit your photos and videos using software like Adobe Photoshop, Adobe Premier, iPhoto, and iMovie
-<br>•&nbsp;Navigate your computer’s basic functionalities with ease</span>
-<br>•&nbsp;Use tools like email and web browsing
-                
-<br><br>Don’t see what you want on the list? Request a free consultation using the contact form , and one of our gurus will be more than happy to help.</p>
-            </div>
-          </div>
-          <div class = "four columns">
-              <div id = "repairLess">
-               <p>Have a trusted piece of technology that just isn’t working the way it used to? Let us know what the problem is, and we’ll take care of the rest.</p>
-              </div>
-              <div id = "repairMore" style="display:none;text-align:left;">
-                <p>Our tech gurus will take care of:
-<br><br>•&nbsp;Speeding up slow computers
-<br>•&nbsp;Recovering/replacing broken or damaged hard-drives
-<br>•&nbsp;Virus removal (and anti-virus installation)
-                
-<br><br>Don’t see what you want on the list? Request a free consultation using the contact form below, and one of our gurus will be more than happy to help.</p>
-              </div>
-          </div>
-          </div>
-        <div class = "row" style = "margin-bottom: 15%;" id = "servicesDesktopButtons">
-          <div class = "four columns" >
-            <button  id="setupBtn">Show More</button>
+            <h5>We Help With</h5>
           </div>
 
-          <div class = "four columns">
-            <button  id="lessonBtn">Show More</button>
-          </div>
-          <div class = "four columns">
-            <button  id="repairBtn">Show More</button>
-          </div>
-        </div> <!-- End Row -->
+          <div class="row logos">
+            <picture>
+              <img class="windows" src = "images/WindowsLogo.png" alt="Windows">
+            </picture>
 
-        <!-- Desktop services end -->
+            <picture>
+              <img class="apple" src = "images/AppleLogo.png" alt="Apple">
+            </picture>
+
+          </div>    
+        </div>
       </div> <!-- End Container -->
   </div> <!-- End Band -->
 
